4ba9e39acfcd05544a66fe5708012a48aa6fd5b1
NotAMojangDev <86620443+NotAMojangDev@users.noreply.github.com>
2024-09-26 22:41:49 +0300

    Fix homepage

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import { Hono } from 'hono'
 import { env } from 'hono/adapter'
-import newLink from './path/new_link'
+import homepage from './path/homepage'
 import handleRedirect from './path/redirect'
 import createLinkEndpoint from './path/create'
 import { whichEndpoint, whichPath } from './path/which'
 
 const app = new Hono<{ Bindings: CloudflareBindings }>()
 
-app.get('/', (ctx) => newLink(ctx))
+app.get('/', (ctx) => homepage(ctx))
 
 app.post('/new', (ctx) => createLinkEndpoint(ctx, ctx.env.LINKS_DB))
 
