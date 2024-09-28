b4f050cd4d27d581fc24e415418f01008aa4fbaa
NotAMojangDev <86620443+NotAMojangDev@users.noreply.github.com>
2024-09-28 11:03:28 +0300

    Add some checks to save storage

--- src/path/create.ts
+++ src/path/create.ts
@@ -4,28 +4,39 @@ import Queries from "../util/queries";
 import isValidUrl from "../util/validateURL";
 
 export default async function createLinkEndpoint(ctx: Context, db: D1Database) {
-    let url: string | undefined = ctx.req.query("url")
-    
-    if (url === undefined) return new Response("No URL provided!", {status: 400})
-    
-    if (!isValidUrl(url)) return new Response("Invalid URL!", {status: 400})
-    
-    let Query = Queries.getURL;
-    let iter = 0;
-    let id: String;
-    do {
-        if (iter === 10) {
-            id = generateIDCustom(11)
-            break;
-        }
-        id = generateStandardID();
-        var result = await db.prepare(Query).bind(id).first("url")
-        iter++;
-    } while (result !== "") 
-    
-    let writeQuery: string = Queries.saveURL;
-    
-    await db.prepare(writeQuery).bind(id, url).run()
-    
-    return new Response("https://s.namd.dev/" + id)
+    let url: string | undefined = ctx.req.query("url");
+
+    if (url === undefined)
+        return new Response("No URL provided!", { status: 400 });
+
+    if (!isValidUrl(url)) return new Response("Invalid URL!", { status: 400 });
+
+    let existingIDsForURL = await db
+        .prepare(Queries.getIDbyURL)
+        .bind(url)
+        .first("id");
+
+    let id;
+
+    if (existingIDsForURL == null) {
+        let query = Queries.getURL;
+        let iter = 0;
+        id;
+        do {
+            if (iter === 10) {
+                id = generateIDCustom(11);
+                break;
+            }
+            id = generateStandardID();
+            // @ts-ignore
+            var result = await db.prepare(query).bind(id).first("url");
+            iter++;
+        } while (result !== "");
+
+        let writeQuery: string = Queries.saveURL;
+
+        await db.prepare(writeQuery).bind(id, url).run();
+    } else id = existingIDsForURL;
+
+    return new Response("https://s.namd.dev/" + id);
 }
--- src/util/queries.ts
+++ src/util/queries.ts
@@ -1,6 +1,11 @@
+const table = "stored_urls";
+const idColumn = "id"
+const urlColumn = "url"
+
 const Queries = {
-    getURL: "SELECT url FROM stored_urls WHERE id = ?",
-    saveURL: "INSERT INTO stored_urls (id, url) VALUES (?, ?)",
+    getURL: `SELECT ${urlColumn} FROM ${table} WHERE ${idColumn} = ?`,
+    saveURL: `INSERT INTO ${table} (${idColumn}, ${urlColumn}) VALUES (?, ?)`,
+    getIDbyURL: `SELECT ${idColumn} FROM ${table} WHERE ${urlColumn} = ?`
 }
 
 export default Queries;
