5e1a37f07ab2c27cb8ffea1cf4c32da16e0c426c
NotAMojangDev <86620443+NotAMojangDev@users.noreply.github.com>
2024-09-28 10:34:55 +0300

    Fixed breaking URL creation

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-import { Hono } from 'hono'
-import { env } from 'hono/adapter'
-import homepage from './path/homepage'
-import handleRedirect from './path/redirect'
-import createLinkEndpoint from './path/create'
-import { whichEndpoint, whichPath } from './path/which'
+import { Hono } from "hono";
+import { env } from "hono/adapter";
+import homepage from "./path/homepage";
+import handleRedirect from "./path/redirect";
+import createLinkEndpoint from "./path/create";
+import { whichEndpoint, whichPath } from "./path/which";
 
-const app = new Hono<{ Bindings: CloudflareBindings }>()
+const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.get('/', (ctx) => homepage(ctx))
+app.get("/", (ctx) => homepage(ctx));
 
-app.post('/new', (ctx) => createLinkEndpoint(ctx, ctx.env.LINKS_DB))
+app.get("/new", (ctx) => createLinkEndpoint(ctx, ctx.env.LINKS_DB));
 
-app.get('/which', (ctx) => whichEndpoint(ctx, ctx.env.LINKS_DB))
+app.get("/which", (ctx) => whichEndpoint(ctx, ctx.env.LINKS_DB));
 
-app.get('/which/*', (ctx) => whichPath(ctx, ctx.env.LINKS_DB))
+app.get("/which/*", (ctx) => whichPath(ctx, ctx.env.LINKS_DB));
 
-app.get('/*', (ctx) => handleRedirect(ctx, ctx.env.LINKS_DB))
+app.get("/*", (ctx) => handleRedirect(ctx, ctx.env.LINKS_DB));
 
-export default app
+export default app;
